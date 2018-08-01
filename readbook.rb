327d2a5de9e3c02e69ca5aa38baafad1f6b687a6
MakotoInomata <inomar1486@gmail.com>
2018-08-01 17:40:38 +0900

    upadte

--- readbook.rb
+++ readbook.rb
@@ -8,5 +8,7 @@ kindle = KindleHighlights::Client.new(
 
 kindle.books.each do |book|
   puts book.to_s
+  # -> ゼロ秒思考 by 赤羽 雄二
   puts book.highlights_from_amazon
+  # -> 頭を使わない状況で成長することはない。考えて物事を整理し、問題点を解決していかなければ、気持ちも晴れることがない。
 end
